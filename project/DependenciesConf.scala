ab0d38cdb78fe87adc26645289def041b56b5c97
Merlijn Boogerd <merlijn.boogerd@trivento.nl>
2017-04-15 08:34:41 +0200

    Updated dependencies (scalaz, logback, scalacheck)

--- project/DependenciesConf.scala
+++ project/DependenciesConf.scala
@@ -18,13 +18,13 @@ object DependenciesConf {
   )
 
   def commonDeps = Seq(
-    "org.scalaz" %% "scalaz-core" % "7.2.8",
+    "org.scalaz" %% "scalaz-core" % "7.2.11",
     "commons-io" % "commons-io" % "2.5",
     "com.iheart" %% "ficus" % "1.4.0",
-    "ch.qos.logback" % "logback-classic" % "1.2.1",
+    "ch.qos.logback" % "logback-classic" % "1.2.3",
 
     "org.scalatest" %% "scalatest" % "3.0.1" % Test,
-    "org.scalacheck" %% "scalacheck" % "1.13.4" % Test,
-    "org.typelevel" %% "scalaz-scalatest" % "1.1.1" % Test
+    "org.scalacheck" %% "scalacheck" % "1.13.5" % Test,
+    "org.typelevel" %% "scalaz-scalatest" % "1.1.2" % Test
   )
 }
